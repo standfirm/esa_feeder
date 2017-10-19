c01bd3f3e68c8cf194af3b216e8dcb9225b57464
corocn <crsk.plmzh@gmail.com>
2017-10-19 17:01:10 +0900

    slack通知するようにした

--- lib/esa_feeder/runner.rb
+++ lib/esa_feeder/runner.rb
@@ -1,25 +1,32 @@
 require 'dotenv/load'
 require 'esa'
+require 'slack-notifier'
 
 module EsaFeeder
   class Runner
     def initialize
-      @client ||= Esa::Client.new(access_token: ENV['ESA_OWNER_API_TOKEN'], current_team: ENV['ESA_TEAM'])
+      @client = Esa::Client.new(access_token: ENV['ESA_OWNER_API_TOKEN'], current_team: ENV['ESA_TEAM'])
+      @notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']) if ENV['SLACK_WEBHOOK_URL']
     end
 
     def run(time: nil, user: 'esa_bot')
       time ||= Time.now
       client.posts(q: query(time)).body['posts'].map do |post|
-        client.create_post(template_post_id: post['number'], user: user)
+        r = client.create_post(template_post_id: post['number'], user: user)
+        notify_slack(r.body['full_name'], r.body['url']) if notifier
       end
     end
 
     private
 
-    attr_reader :client
+    attr_reader :client, :notifier
 
     def query(time)
       "in:templates tag:feed_#{time.strftime('%a').downcase}"
+    end
+
+    def notify_slack(title, url)
+      notifier.post text: "#{title}を作成しました / #{url}"
     end
   end
 end
