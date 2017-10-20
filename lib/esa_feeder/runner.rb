e9d909fb934aa4b78c0e28c675b37771aed41d2a
kokuyouwind <kokuyouwind@gmail.com>
2017-10-20 17:53:22 +0900

    SlackClient Gatewayを切り出した

--- lib/esa_feeder/runner.rb
+++ lib/esa_feeder/runner.rb
@@ -6,14 +6,14 @@ module EsaFeeder
   class Runner
     def initialize
       @client = EsaFeeder::Gateways::EsaClient.new(ENV['ESA_OWNER_API_TOKEN'], ENV['ESA_TEAM'])
-      @notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']) if ENV['SLACK_WEBHOOK_URL']
+      @notifier = EsaFeeder::Gateways::SlackClient.new(ENV['SLACK_WEBHOOK_URL']) if ENV['SLACK_WEBHOOK_URL']
     end
 
     def run(time: nil, user: 'esa_bot')
       time ||= Time.now
       client.find_templates(source_tag(time)).map do |template|
         post = client.create_from_template(template, user)
-        notify_slack(post.full_name, post.url) if notifier
+        notifier&.notify_creation('新しい記事を作成しました', post)
         { template.number => post.number }
       end
     end
@@ -21,15 +21,6 @@ module EsaFeeder
     private
 
     attr_reader :client, :notifier
-
-    def notify_slack(title, url)
-      notifier.post attachments: [{
-        pretext: '新しい記事を作成しました',
-        title: title,
-        title_link: url,
-        color: 'good'
-      }]
-    end
 
     def source_tag(time)
       "feed_#{time.strftime('%a').downcase}"
