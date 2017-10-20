318cac1b272ac79bbf77e09be56995f88cdbe437
kokuyouwind <kokuyouwind@gmail.com>
2017-10-20 17:34:58 +0900

    EsaClient Gatewayを切り出した

---
+++ lib/esa_feeder/gateways/esa_client.rb
@@ -0,0 +1,35 @@
+module EsaFeeder
+  module Gateways
+    class EsaClient
+      def initialize(access_token, current_team)
+        @client = Esa::Client.new(access_token: access_token, current_team: current_team)
+      end
+
+      def find_templates(tag)
+        response = client.posts(q: "in:templates tag:#{tag}")
+        to_entities(response.body)
+      end
+
+      def create_from_template(post, user)
+        response = client.create_post(template_post_id: post.number, user: user)
+        to_entity(response.body)
+      end
+
+      private
+
+      attr_reader :client
+
+      def to_entities(body)
+        body['posts'].map { |raw| to_entity(raw) }
+      end
+
+      def to_entity(raw)
+        EsaFeeder::Entities::EsaPost.new(
+          raw['number'],
+          raw['full_name'],
+          raw['url']
+        )
+      end
+    end
+  end
+end
--- lib/esa_feeder/runner.rb
+++ lib/esa_feeder/runner.rb
@@ -5,29 +5,22 @@ require 'slack-notifier'
 module EsaFeeder
   class Runner
     def initialize
-      @client = Esa::Client.new(access_token: ENV['ESA_OWNER_API_TOKEN'], current_team: ENV['ESA_TEAM'])
+      @client = EsaFeeder::Gateways::EsaClient.new(ENV['ESA_OWNER_API_TOKEN'], ENV['ESA_TEAM'])
       @notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']) if ENV['SLACK_WEBHOOK_URL']
     end
 
     def run(time: nil, user: 'esa_bot')
       time ||= Time.now
-      client.posts(q: query(time)).body['posts'].map do |raw|
-        post = to_esa_entity(raw)
-        r = to_esa_entity(
-          client.create_post(template_post_id: post.number, user: user).body
-        )
-        notify_slack(r.full_name, r.url) if notifier
-        { post.number => r.number }
+      client.find_templates(source_tag(time)).map do |template|
+        post = client.create_from_template(template, user)
+        notify_slack(post.full_name, post.url) if notifier
+        { template.number => post.number }
       end
     end
 
     private
 
     attr_reader :client, :notifier
-
-    def query(time)
-      "in:templates tag:feed_#{time.strftime('%a').downcase}"
-    end
 
     def notify_slack(title, url)
       notifier.post attachments: [{
@@ -38,12 +31,8 @@ module EsaFeeder
       }]
     end
 
-    def to_esa_entity(raw)
-      EsaFeeder::Entities::EsaPost.new(
-        raw['number'],
-        raw['full_name'],
-        raw['url']
-      )
+    def source_tag(time)
+      "feed_#{time.strftime('%a').downcase}"
     end
   end
 end
