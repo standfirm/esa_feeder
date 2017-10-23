bae7102c63d4acb5e341e8da4f0ae473d8ccb932
kokuyouwind <kokuyouwind@gmail.com>
2017-10-23 14:26:24 +0900

    EsaClient::create_from_templateのspecを追加した

--- spec/gateways/esa_client_spec.rb
+++ spec/gateways/esa_client_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 RSpec.describe EsaFeeder::Gateways::EsaClient do
   let(:driver) { double('driver') }
   let(:target) { described_class.new(driver) }
+  let(:template) {
+    EsaFeeder::Entities::EsaPost.new(
+      99999,
+      'templates/category/test #feed_mon',
+      'https://example.com/posts/99999'
+    ) }
 
   describe '#find_templates' do
     let(:body) do
@@ -13,19 +19,37 @@ RSpec.describe EsaFeeder::Gateways::EsaClient do
       ]}
     end
     let(:response) { double('response', body: body) }
-    let(:post) {
-      EsaFeeder::Entities::EsaPost.new(
-        99999,
-        'templates/category/test #feed_mon',
-        'https://example.com/posts/99999'
-      ) }
 
     subject { target.find_templates('test_tag') }
 
     it 'return templates' do
       allow(driver).to receive(:posts).with(q: 'in:templates tag:test_tag')
         .and_return(response)
-      expect(subject).to eq([post])
+      expect(subject).to eq([template])
+    end
+  end
+
+  describe '#create_from_template' do
+    let(:body) do
+      { 'number' => 12345,
+        'full_name' => 'category/test #feed_mon',
+        'url' => 'https://example.com/posts/12345' }
+    end
+    let(:response) { double('response', body: body) }
+    let(:post) {
+      EsaFeeder::Entities::EsaPost.new(
+        12345,
+        'category/test #feed_mon',
+        'https://example.com/posts/12345'
+      ) }
+
+    subject { target.create_from_template(template, 'bot_user') }
+
+    it 'return created post' do
+      allow(driver).to receive(:create_post)
+        .with(template_post_id: 99999, user: 'bot_user')
+        .and_return(response)
+      expect(subject).to eq(post)
     end
   end
 end
