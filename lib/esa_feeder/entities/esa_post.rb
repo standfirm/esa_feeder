27ff09a737b529cce0d3aaa01b61bf6a40f880d9
kokuyouwind <kokuyouwind@gmail.com>
2017-10-24 16:15:20 +0900

    EsaPost#slack_channnelsからslack_tagsを切り出し

--- lib/esa_feeder/entities/esa_post.rb
+++ lib/esa_feeder/entities/esa_post.rb
@@ -4,12 +4,17 @@ module EsaFeeder
   module Entities
     EsaPost = Struct.new(:number, :category, :name, :url, :tags) do
       def slack_channels
-        tags.select { |tag| tag =~ /^slack_/ }
-            .map { |tag| tag.gsub(/^slack_/, '') }
+        slack_tags.map { |tag| tag.gsub(/^slack_/, '') }
       end
 
       def full_name
         category ? "#{category}/#{name}" : name
+      end
+
+      private
+
+      def slack_tags
+        tags.select { |tag| tag =~ /^slack_/ }
       end
     end
   end
