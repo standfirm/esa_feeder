9e6fc5806749a2f12c658b57dc8f6e27d405db60
corocn <crsk.plmzh@gmail.com>
2017-10-24 17:28:42 +0900

    rubocopが怒るので修正

--- spec/use_cases/source_tag_spec.rb
+++ spec/use_cases/source_tag_spec.rb
@@ -3,8 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe EsaFeeder::UseCases::SourceTag do
-  let(:tags) { (0..6).map { |n| described_class.new( Time.local(2017, 1, 1 + n) ).call} }
-  let(:ref) {['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']}
+  let(:tags) do
+    (0..6).map { |n| described_class.new(Time.local(2017, 1, 1 + n)).call }
+  end
+
+  let(:ref) { %w[sun mon tue wed thu fri sat] }
 
   it 'check tag each day of the week' do
     (0..6).map { |n| expect(tags[n]).to eq("feed_#{ref[n]}") }
