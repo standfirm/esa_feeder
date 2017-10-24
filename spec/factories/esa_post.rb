e6603e933d8f7c74bcc9ffefa01ced01e701f716
kokuyouwind <kokuyouwind@gmail.com>
2017-10-24 12:10:15 +0900

    EsaPostのfactoryを追加

---
+++ spec/factories/esa_post.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :esa_post, class: EsaFeeder::Entities::EsaPost do
+    sequence :number
+    full_name { "path/to/post/post_title_#{number} #feed_mon" }
+    url { "https://example.com/posts/#{number}" }
+    tags %w[tag]
+
+    factory :esa_template do
+      tags %w[tag feed_mon]
+      full_name { "templates/path/to/post/post_title_#{number} #feed_mon" }
+
+      trait :with_slack_tag do
+        tags %w[tag feed_mon slack_hoge hoge_slack_fuga]
+      end
+    end
+  end
+end
