c8ac74ac4452c28933005df42d11764c88922a30
corocn <crsk.plmzh@gmail.com>
2017-10-24 13:58:49 +0900

    先頭のslack_のみに反応するように正規表現書き換え

--- lib/esa_feeder/entities/esa_post.rb
+++ lib/esa_feeder/entities/esa_post.rb
@@ -3,7 +3,7 @@ module EsaFeeder
     class EsaPost < Struct.new(:number, :full_name, :url, :tags)
       def slack_channels
         return nil if tags.nil?
-        tags.select { |tag| tag=~ /slack_/ }.map { |tag| tag.gsub(/slack_/, '') }
+        tags.select { |tag| tag=~ /^slack_/ }.map { |tag| tag.gsub(/^slack_/, '') }
       end
     end
   end
