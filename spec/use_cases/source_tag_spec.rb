c97e9f79016605c4bf488d120affab257975c9e1
corocn <crsk.plmzh@gmail.com>
2017-10-24 17:24:42 +0900

    1週間分チェックするようにした

--- spec/use_cases/source_tag_spec.rb
+++ spec/use_cases/source_tag_spec.rb
@@ -3,11 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe EsaFeeder::UseCases::SourceTag do
-  let(:monday) { Time.local(2017, 10, 23) }
+  let(:tags) { (0..6).map { |n| described_class.new( Time.local(2017, 1, 1 + n) ).call} }
+  let(:ref) {['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']}
 
-  subject { described_class.new(monday).call }
-
-  it do
-    expect(subject).to eq('feed_mon')
+  it 'check tag each day of the week' do
+    (0..6).map { |n| expect(tags[n]).to eq("feed_#{ref[n]}") }
   end
 end
