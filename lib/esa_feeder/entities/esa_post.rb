0b2907ca5ce056c8f4f7c469b416c86571252c22
kokuyouwind <kokuyouwind@gmail.com>
2017-10-24 16:08:39 +0900

    Entities::EsaPost にfull_nameの代わりにcategoryとnameを持たせるようにした
    full_nameはメソッドにした
    full_nameからタグの情報が抜けたが、もともと通知にしか使っていないため、
    仕様をこちらに合わせる

--- lib/esa_feeder/entities/esa_post.rb
+++ lib/esa_feeder/entities/esa_post.rb
@@ -2,10 +2,14 @@
 
 module EsaFeeder
   module Entities
-    EsaPost = Struct.new(:number, :full_name, :url, :tags) do
+    EsaPost = Struct.new(:number, :category, :name, :url, :tags) do
       def slack_channels
         tags.select { |tag| tag =~ /^slack_/ }
             .map { |tag| tag.gsub(/^slack_/, '') }
+      end
+
+      def full_name
+        category ? "#{category}/#{name}" : name
       end
     end
   end
