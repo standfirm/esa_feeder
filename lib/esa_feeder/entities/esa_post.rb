6ab435ea1604372402fafc34fe477c06af09c63d
corocn <crsk.plmzh@gmail.com>
2017-10-24 12:11:42 +0900

    EsaPost#slack_channelsでチャンネル一覧を取得できるようにした

--- lib/esa_feeder/entities/esa_post.rb
+++ lib/esa_feeder/entities/esa_post.rb
@@ -1,5 +1,10 @@
 module EsaFeeder
   module Entities
-    EsaPost = Struct.new(:number, :full_name, :url)
+    class EsaPost < Struct.new(:number, :full_name, :url, :tags)
+      def slack_channels
+        return nil if tags.nil?
+        tags.select { |tag| tag=~ /slack_/ }.map { |tag| tag.gsub(/slack_/, '') }
+      end
+    end
   end
 end
