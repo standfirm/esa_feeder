612b5d3ad167d5ba1d840edd463bea5f484c6ed0
corocn <crsk.plmzh@gmail.com>
2017-10-24 13:58:49 +0900

    先頭のslack_のみに反応するように正規表現書き換え

--- spec/entities/esa_post_spec.rb
+++ spec/entities/esa_post_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 
 RSpec.describe EsaFeeder::Entities::EsaPost do
-  let(:post) { described_class.new(99999, 'path/to/post/post_title #tag', 'https://example.com/posts/99999', ['feed_mon', 'slack_hoge', 'feed_tue', 'slack_foo']) }
+  let(:post) { described_class.new(99999, 'path/to/post/post_title #tag', 'https://example.com/posts/99999', ['feed_mon', 'slack_hoge', 'feed_tue', 'slack_foo', 'hoge_slack_foo']) }
   let(:post_no_tag) { described_class.new(99999, 'path/to/post/post_title #tag', 'https://example.com/posts/99999') }
 
   it 'can assign values' do
     expect(post.number).to eq(99999)
     expect(post.full_name).to eq('path/to/post/post_title #tag')
     expect(post.url).to eq('https://example.com/posts/99999')
-    expect(post.tags).to eq(['feed_mon', 'slack_hoge', 'feed_tue', 'slack_foo'])
+    expect(post.tags).to eq(['feed_mon', 'slack_hoge', 'feed_tue', 'slack_foo', 'hoge_slack_foo'])
   end
 
   describe '#slack_channels' do
