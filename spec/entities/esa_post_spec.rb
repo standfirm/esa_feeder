4b3e67390d0e18dd805f1196246ace1cbe43c860
kokuyouwind <kokuyouwind@gmail.com>
2017-10-24 14:47:43 +0900

    Entities::EsaPost specをリファクタリング

--- spec/entities/esa_post_spec.rb
+++ spec/entities/esa_post_spec.rb
@@ -1,26 +1,32 @@
 require "spec_helper"
 
 RSpec.describe EsaFeeder::Entities::EsaPost do
-  let(:post) { described_class.new(99999, 'path/to/post/post_title #tag', 'https://example.com/posts/99999', ['feed_mon', 'feed_tue']) }
-  let(:post_with_slack_tag) { described_class.new(99999, 'path/to/post/post_title #tag', 'https://example.com/posts/99999', ['feed_mon', 'slack_hoge', 'hoge_slack_foo', 'slack_foo']) }
+  describe '#initialize' do
+    let(:post) do
+      described_class.new(99999, 'path/to/post/post_title #tag',
+        'https://example.com/posts/99999', %w[feed_mon feed_tue])
+    end
 
-  it 'can assign values' do
-    expect(post.number).to eq(99999)
-    expect(post.full_name).to eq('path/to/post/post_title #tag')
-    expect(post.url).to eq('https://example.com/posts/99999')
-    expect(post.tags).to eq(['feed_mon', 'feed_tue'])
+    it 'can assign values' do
+      expect(post.number).to eq(99999)
+      expect(post.full_name).to eq('path/to/post/post_title #tag')
+      expect(post.url).to eq('https://example.com/posts/99999')
+      expect(post.tags).to eq(%w[feed_mon feed_tue])
+    end
   end
 
   describe '#slack_channels' do
+    subject { post.slack_channels }
+
     context 'with slack tags' do
-      subject { post_with_slack_tag.slack_channels }
+      let(:post) { build(:esa_template, :with_slack_tag) }
       it 'return only slack tag' do
-        expect(subject).to eq(['hoge', 'foo'])
+        expect(subject).to eq(['hoge'])
       end
     end
 
     context 'with no slack tags' do
-      subject { post.slack_channels }
+      let(:post) { build(:esa_template) }
       it 'return empty array' do
         expect(subject).to eq([])
       end
