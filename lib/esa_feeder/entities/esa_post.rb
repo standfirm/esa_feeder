f339c0a75d17c874d9522028e12a09383ed5267b
corocn <crsk.plmzh@gmail.com>
2017-10-24 14:08:04 +0900

    tagsはnilにはならないので、条件を消した

--- lib/esa_feeder/entities/esa_post.rb
+++ lib/esa_feeder/entities/esa_post.rb
@@ -2,7 +2,6 @@ module EsaFeeder
   module Entities
     class EsaPost < Struct.new(:number, :full_name, :url, :tags)
       def slack_channels
-        return nil if tags.nil?
         tags.select { |tag| tag=~ /^slack_/ }.map { |tag| tag.gsub(/^slack_/, '') }
       end
     end
