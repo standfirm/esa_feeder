a91144ea663711fc99626e7cd866ed79abfb2cb7
Pegasus204 <ride.or.die.2215@gmail.com>
2018-01-18 00:35:05 +0900

    指定した期間が経過した記事をアーカイブできることを検証するspecを追加した

--- spec/entities/esa_post_spec.rb
+++ spec/entities/esa_post_spec.rb
@@ -93,4 +93,19 @@ RSpec.describe EsaFeeder::Entities::EsaPost do
       it { expect(subject).to eq(%w[piyo]) }
     end
   end
+
+  describe '#archived_category' do
+    let(:post) { build(:esa_post, category: category) }
+    subject { post.archived_category }
+
+    context 'not contain category' do
+      let(:category) { nil }
+      it { expect(subject).to eq('Archived/') }
+    end
+
+    context 'contain category' do
+      let(:category) { 'path/to/post' }
+      it { expect(subject).to eq('Archived/path/to/post') }
+    end
+  end
 end
