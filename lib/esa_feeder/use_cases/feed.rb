c8aaa37c6e6cf960ea6d98c1f48c9b546947df1d
kokuyouwind <kokuyouwind@gmail.com>
2019-04-04 15:36:42 +0900

    :art: extract feed_post method

--- lib/esa_feeder/use_cases/feed.rb
+++ lib/esa_feeder/use_cases/feed.rb
@@ -11,12 +11,7 @@ module EsaFeeder
       def call(tags)
         find_templates(tags).map do |template|
           template.feed_users.map do |feed_user|
-            post = esa_port.create_from_template(template, feed_user)
-            notifier_port&.notify_creation('新しい記事を作成しました', post)
-            # remove system tags from generated post
-            post.tags = post.user_tags
-            esa_port.update_post(post, feed_user)
-            { template.number => post.number }
+            feed_post(template, feed_user)
           end
         end.flatten
       end
@@ -29,6 +24,15 @@ module EsaFeeder
         tags.map do |tag|
           esa_port.find_templates(tag)
         end.flatten
+      end
+
+      def feed_post(template, feed_user)
+        post = esa_port.create_from_template(template, feed_user)
+        notifier_port&.notify_creation('新しい記事を作成しました', post)
+        # remove system tags from generated post
+        post.tags = post.user_tags
+        esa_port.update_post(post, feed_user)
+        { template.number => post.number }
       end
     end
   end
